03b518adf9253e7fa50bbe12a02fc077567c1d19
Qrcze <19560677+Qrcze@users.noreply.github.com>
2025-06-20 23:10:50 +0200

    removed keyboard hook, as it's handled by windows and linux media player APIs

--- SoundMist/App.axaml.cs
+++ SoundMist/App.axaml.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
-using SoundMist.Models;
-using SoundMist.ViewModels;
 using SoundMist.Views;
-using System.Net.Http;
-using SoundMist.Models.Audio;
+using System;
 
 namespace SoundMist;
 
 public partial class App : Application
 {
     public static event Action<ServiceProvider>? ServiceConfigured;
+
     private static ServiceProvider services = null!;
 
     public static T GetService<T>() where T : notnull
@@ -39,18 +36,6 @@ public partial class App : Application
         services = collection.BuildServiceProvider();
 
         ServiceConfigured?.Invoke(services);
-
-#if !OS_LINUX
-        KeyboardHook.Run();
-
-        var musicPlayer = services.GetRequiredService<IMusicPlayer>();
-
-        KeyboardHook.PlayPausedTriggered += musicPlayer.PlayPause;
-        KeyboardHook.PlayTriggered += musicPlayer.Play;
-        KeyboardHook.PauseTriggered += musicPlayer.Pause;
-        KeyboardHook.PrevTrackTriggered += () => System.Threading.Tasks.Task.Run(async () => await musicPlayer.PlayPrev());
-        KeyboardHook.NextTrackTriggered += () => System.Threading.Tasks.Task.Run(async () => await musicPlayer.PlayNext());
-#endif
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
